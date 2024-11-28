a4b4682b0eb49d1795cda5946542b804fb5bd2cf
GrayHead <ss.zhuravlov@gmail.com>
2024-11-28 19:32:57 +0200

    useEffect & fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
-import {useState} from "react";
+import UsersComponent from "./components/users-component/UsersComponent.tsx";
 
 const App = () => {
 
 
-    let [counter, setCounter] = useState<number>(0);
-    console.log('mount');
     return (
         <div>
-
-            <h2>{counter}</h2>
-            <button onClick={() => {
-                setCounter(++counter);
-            }}>increment
-            </button>
-
-            <button onClick={() => {
-                setCounter(prevState => {
-                    return --prevState;
-                });
-            }}>decrement
-            </button>
+            <UsersComponent/>
         </div>
     );
 };
