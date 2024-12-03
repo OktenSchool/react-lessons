b3255e3c62f9f8ac5cc5275ff6cc6f81903b62f1
GrayHead <ss.zhuravlov@gmail.com>
2024-12-03 17:55:09 +0200

    lesson 3-4 useForm JOI validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import {FC} from 'react';
+import  {FC} from 'react';
 import './App.css';
 import FormComponent from "./components/FormComponent";
 
 const App: FC = () => {
     return (
         <>
-            <FormComponent/>
+          <FormComponent/>
         </>
 
     );
