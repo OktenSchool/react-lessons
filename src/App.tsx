fb094c49068949eecc89c04c8e6b9ecea2e81a26
GrayHead <ss.zhuravlov@gmail.com>
2024-12-18 16:41:19 +0200

    context

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import './App.css';
+import {LeftBranch} from "./components/LeftBranch.tsx";
+import {RightBranch} from "./components/RightBranch.tsx";
+import {MyContext} from "./contetx/MyContext.tsx";
+import {useState} from "react";
 
 function App() {
 
+    const [counter, setCounter] = useState<number>(0);
 
     return (
         <>
+
+            <MyContext.Provider value={{
+                counterValue: counter,
+                increment: (obj) => {
+                    setCounter(++obj);
+                }
+            }}>
+
+                <LeftBranch/>
+                <RightBranch/>
+            </MyContext.Provider>
 
         </>
     );
