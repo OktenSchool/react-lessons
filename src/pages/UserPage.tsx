895a5ed1f12c4706a488f6fe361e02be19e325ee
GrayHead <ss.zhuravlov@gmail.com>
2024-12-19 12:05:35 +0200

    start redux

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,23 +1,9 @@
-import {useParams} from "react-router-dom";
-import {useEffect} from "react";
-import {useAppDispatch, useAppSelector} from "../redux/store.ts";
-import {userActions} from "../redux/slices/userSlice.tsx";
-
 export const UserPage = () => {
-    const {id} = useParams();
-
-    const dispatch = useAppDispatch();
-    const {userSlice: {user}} = useAppSelector(state => state);
-    useEffect(() => {
-        if (id) {
-            dispatch(userActions.loadUser(+id));
-        }
-    }, [id]);
 
 
     return (
         <div>
-            {user && <div>{user.name}</div>}
+
         </div>
     );
 };
