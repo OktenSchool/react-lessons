d0cfe51c965e11ddda3ef2f2a19fd8ef2c022c60
GrayHead <ss.zhuravlov@gmail.com>
2024-12-31 16:57:55 +0200

    task 3 - 3 resolve knZgVIwvqd

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,7 +1,9 @@
 import {Outlet} from "react-router";
+import {MenuConponent} from "../components/menu/MenuConponent.tsx";
 
 export const MainLayout = () => {
     return <div>
+        <MenuConponent/>
         <Outlet/>
     </div>
 };
