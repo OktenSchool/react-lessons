b42cf89c5c39a5205135e84f6cd5baa0a94e59cb
GrayHead <ss.zhuravlov@gmail.com>
2024-12-31 16:49:49 +0200

    task 3 - 2 resolve z5PFSTAK

---
+++ src/components/user-component/UserComponent.tsx
@@ -0,0 +1,22 @@
+import {useNavigate} from "react-router";
+import {FC} from "react";
+import {IUser} from "../../models/IUser.ts";
+
+type Props = {
+    user: IUser,
+
+}
+export const UserComponent: FC<Props> = ({user}) => {
+    const navigation = useNavigate();
+
+    const onButtonClickNavigate = () => {
+        navigation('/users/' + user.id + '/carts')
+    }
+    return <div className={"my-5"}>
+        {user.username}: {user.email}
+
+
+        <button className={"border-2"} onClick={onButtonClickNavigate}>click me
+        </button>
+    </div>;
+}
